0657f57f8fce1b3912f09aec0a635319f426b9a5
Nir y <Niry27@gmail.com>
2023-05-08 21:17:51 +0300

    Added axios

--- Shows-Api/app.js
+++ Shows-Api/app.js
@@ -1,14 +1,11 @@
-const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const postsRoutes = require("./routes/posts");
-const userRoutes = require("./routes/user");
+const axios = require("axios");
 
 const app = express();
 
 
-const connectToMyMongo = require("./MongoConnection");
+const connectToMyMongo = require("./connectDb");
 
 
 ////// invoke connection to mongoDB
@@ -18,8 +15,6 @@ connectToMyMongo();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")));
-
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -35,8 +30,48 @@ app.use((req, res, next) => {
 });
 
 
-app.get("/api/health", (req, res, next) => { res.status(201).json({ health: "Online ! :)" }); })
-app.use("/api/posts", postsRoutes);
-app.use("/api/user", userRoutes);
+app.get("/api/health", (req, res, next) => { res.status(200).json({ health: "Online ! :)" }); })
+
+app.get("/api/test", async (req, res, next) => {
+
+
+    let responseApi = "none";
+
+    const searchShow = "inception 2010";
+
+    const urlApi = `https://imdb-api.com/en/API/Search/${process.env.ApiKey}/${searchShow}`;
+
+    console.log(urlApi);
+
+    // req from rate api
+    await axios({
+        method: 'get',
+        url: urlApi,
+    })
+        .then((response) => {
+            // rateCAN = response.data.rates.CAD;
+            // rateEuro = response.data.rates.EUR;
+
+            responseApi = response;
+            // console.log(response);
+
+
+        }).catch(e => {
+            console.log({
+                message: "oops :(",
+                error: e,
+            })
+        });
+
+    responseApi = responseApi.data;
+
+    res.status(200).json({
+        health: "Online ! :)",
+        responseApi
+    });
+})
+
+
+
 
 module.exports = app;
